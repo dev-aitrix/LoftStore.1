4d7a35ece6133e017ad09fd98e04a4c3b3c393cc
Kasymov Azamat <kasymov.azamat.kk@gmail.com>
2023-03-03 19:11:27 +0600

    Upgrated serializersUpgrated serializers

--- users/views.py
+++ users/views.py
@@ -1,12 +1,7 @@
-# from rest_framework.response import Response
-# from rest_framework import generics
-# from rest_framework.views import APIView
-#
-# from .models import User
-# from .serializers import UserSerializer
-#
-# class UserAPIView(APIView):
-#     pass
-#     def get(self, request):
-#         users = User.objects.filter(draf=False)
-#         serializers = UserSerializer
+from rest_framework import viewsets
+from users.models import User
+from users.serializers import UserSerializer
+
+class userViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
