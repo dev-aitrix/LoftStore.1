51f53aab7d14636729a49c0e98fd8bf52f80bf56
Kasymov Azamat <kasymov.azamat.kk@gmail.com>
2023-03-13 20:17:53 +0600

    ckeditor and slugs commit

--- LoftStore/urls.py
+++ LoftStore/urls.py
@@ -10,9 +10,13 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
     path('orders/', include('orders.urls')),
+    #  jwt token urls
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    #ckeditor
+    path('ckeditor/', include('ckeditor_uploader.urls'))
 ]
 
 
--- products/models.py
+++ products/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 import uuid
 
+class ProductCategoryModel(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='slug')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
 class ProductModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(verbose_name='Наименование', max_length=1000)
-    category = models.CharField(verbose_name='Категория', max_length=1000)
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='слаг')
+    category = models.ForeignKey(ProductCategoryModel, on_delete=models.PROTECT, related_name='products')
     description = models.TextField(verbose_name='Описание', max_length=20000)
     price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
     count = models.IntegerField(verbose_name='Количество',)
-    image = models.ImageField(upload_to='images/%Y-%m-%d', blank=True, null=True, verbose_name='Картинки продукта')
+    image = models.ImageField(upload_to='product_images/%Y-%m-%d', blank=True, null=True, verbose_name='Картинки продукта')
 
     class Meta:
         verbose_name = 'Товар'
